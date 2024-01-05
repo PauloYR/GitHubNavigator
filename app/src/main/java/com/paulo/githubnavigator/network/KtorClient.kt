13f6cff46d8efa5e9e4fcbad77ee198cfb2d7ae2
Paulo Rocha <pauloyr@outlook.com>
2024-01-05 15:38:34 -0300

    change to use ktor client

---
+++ app/src/main/java/com/paulo/githubnavigator/network/KtorClient.kt
@@ -0,0 +1,64 @@
+package com.paulo.githubnavigator.network
+
+import android.util.Log
+import com.paulo.githubnavigator.BuildConfig
+import io.ktor.client.HttpClient
+import io.ktor.client.engine.android.Android
+import io.ktor.client.plugins.DefaultRequest
+import io.ktor.client.plugins.contentnegotiation.ContentNegotiation
+import io.ktor.client.plugins.defaultRequest
+import io.ktor.client.plugins.logging.LogLevel
+import io.ktor.client.plugins.logging.Logger
+import io.ktor.client.plugins.logging.Logging
+import io.ktor.client.plugins.observer.ResponseObserver
+import io.ktor.client.request.header
+import io.ktor.http.ContentType
+import io.ktor.http.HttpHeaders
+import io.ktor.serialization.kotlinx.json.json
+import kotlinx.serialization.json.Json
+
+private const val TIME_OUT = 60_000
+
+fun createHttpClient(baseUrl: String) = HttpClient(Android) {
+
+    install(ContentNegotiation) {
+        json(
+            Json {
+                ignoreUnknownKeys = true
+            }
+        )
+
+        engine {
+            connectTimeout = TIME_OUT
+            socketTimeout = TIME_OUT
+        }
+    }
+
+    install(Logging) {
+        logger = object : Logger {
+            override fun log(message: String) {
+                Log.v("Logger Ktor =>", message)
+            }
+
+        }
+        level =
+            if (BuildConfig.DEBUG)
+                LogLevel.ALL
+            else
+                LogLevel.NONE
+    }
+
+    install(ResponseObserver) {
+        onResponse { response ->
+            Log.d("HTTP status:", "${response.status.value}")
+        }
+    }
+
+    install(DefaultRequest) {
+        header(HttpHeaders.ContentType, ContentType.Application.Json)
+    }
+
+    defaultRequest {
+        url(baseUrl)
+    }
+}
